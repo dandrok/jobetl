8a89f0965cded499b1cc21fc67020c2dec83057c
dandrok <patryk.stusik@gmail.com>
2026-07-11 20:40:14 +0200

    refactor: use natice timestamptz columns, add database indexes, and harden PM2 process config

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -6,7 +6,13 @@ module.exports = {
       args: "run api",
       env_production: {
         NODE_ENV: "production"
-      }
+      },
+      error_file: "./logs/err.log",
+      out_file: "./logs/out.log",
+      log_date_format: "YYYY-MM-DD HH:mm:ss Z",
+      max_memory_restart: "250M",
+      autorestart: true,
+      exp_backoff_restart_delay: 100
     }
   ]
 };
