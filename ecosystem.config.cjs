2bf1595509b5b55c0a491fca6e8cb9ff19aecd11
dandrok <patryk.stusik@gmail.com>
2026-07-13 18:23:32 +0200

    chore: add daily cron crawler to pm2 configuration

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -13,6 +13,19 @@ module.exports = {
       max_memory_restart: "250M",
       autorestart: true,
       exp_backoff_restart_delay: 100
+    },
+    {
+      name: "jobetl-crawler",
+      script: "npm",
+      args: "run dev",
+      autorestart: false,
+      cron_restart: "0 19 * * *",
+      env_production: {
+        NODE_ENV: "production"
+      },
+      error_file: "./logs/crawler-err.log",
+      out_file: "./logs/crawler-out.log",
+      log_date_format: "YYYY-MM-DD HH:mm:ss Z"
     }
   ]
 };
